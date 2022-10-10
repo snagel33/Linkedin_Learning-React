01edd33c5ebd79f5d887581ef70dea70151212ff
Steven Nagel <stevencnagel@gmail.com>
2022-10-10 16:07:06 -0500

    chapter 4 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [year, setYear] = useState(2020);
+  const [manager, setManager] = useState("Alex");
+  const [status, setStatus] = useState("Open");
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    // alert(`checked: ${checked.toString()}`);
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Year</button>
+      </div>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Rachel")}>New Manager</button>
+      </div>
+      <div className="Status">
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Back in 5")}>Break</button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+      </div>
+      <div>
+        <input 
+          type="checkbox" 
+          value={checked} 
+          onChange={() => setChecked(checked => !checked)}  
+        />
+        {checked ? "checked" : "not checked"}
+      </div>
+    </>
   );
 }
 
+// function Checkbox() {
+//   const [checked, setChecked] = useState(false);
+//   useEffect(() => {
+//     alert(`checked: ${checked.toString()}`);
+//   })
+//   return (
+//     <>
+//       <input 
+//         type="checkbox" 
+//         value={checked} 
+//         onChange={() => setChecked(checked => !checked)}  
+//       />
+//       {checked ? "checked" : "not checked"}
+//     </>
+//   )
+// }
+
 export default App;
+
